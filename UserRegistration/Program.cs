c45cf75aa478a156267b6ff0a8d48794e9a74b01
Shailesh kumar Satapathy <shaileshsatapathy9@gmail.com>
2021-04-24 11:04:13 +0530

    last name added

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -10,6 +10,9 @@ namespace UserRegistration
             Patterns patterns = new Patterns();
             Console.WriteLine("enter the first name:");
             Console.WriteLine(patterns.ValidateFirstName(Console.ReadLine()));
+
+            Console.WriteLine("enter the last name:");
+            Console.WriteLine(patterns.ValidateFirstName(Console.ReadLine()));
         }
     }
 }
